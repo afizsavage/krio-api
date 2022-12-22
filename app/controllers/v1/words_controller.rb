2a524fcd90b25e6ac3477b487b7382cf1116a8a4
Afiz Savage <savageafiz9@gmail.com>
2022-12-22 17:33:01 +0000

    add word with nested attribute for definitions

--- app/controllers/v1/words_controller.rb
+++ app/controllers/v1/words_controller.rb
@@ -6,7 +6,7 @@ class V1::WordsController < ApplicationController
   end
 
   def create
-    @word = Word.create(words_params)
+    @word = Word.create(word_params)
 
     if @word.save
       render json: @word.to_json, status: :created
@@ -17,7 +17,10 @@ class V1::WordsController < ApplicationController
 
   private
 
-  def words_params
-    params.require(:word).permit(:title, :letter_id, :user_id)
+  def word_params
+    params.require(:word).permit(
+      :title, :letter_id, :user_id,
+      defination_attributes: %i[define example_statement approval_status user_id]
+    )
   end
 end

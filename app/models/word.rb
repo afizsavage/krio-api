b4ee58ed5d091a88227195c518e48629731c1cdc
Afiz Savage <savageafiz9@gmail.com>
2022-12-23 15:04:07 +0000

    add full-text search capability

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,7 +1,11 @@
 class Word < ApplicationRecord
+  include PgSearch::Model
+
   belongs_to :user
   belongs_to :letter
   has_one :defination
 
   accepts_nested_attributes_for :defination
+
+  pg_search_scope :search_by_title, against: :title
 end

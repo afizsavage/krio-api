05a9a6ad8948934e9b80a4b71e7f1129122359c2
Afiz Savage <savageafiz9@gmail.com>
2023-03-18 12:49:48 +0000

    correct migrations

--- db/migrate/20230318095942_rollback_column_name_change.rb
+++
@@ -1,6 +0,0 @@
-class RollbackColumnNameChange < ActiveRecord::Migration[7.0]
-  def change
-    rename_column :words, :author_id, :user_id
-    rename_column :definations, :reviewer_id, :user_id
-  end
-end
